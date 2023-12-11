57ed491e08ac4c9cef2e6e0f38a7a78870bb5e26
David Naar <naardavid.t@gmail.com>
2023-12-10 21:45:06 -0500

    fix data processing

--- src/services/btc.ts
+++ src/services/btc.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
+import { quickSort } from '../utils/sorting';
 
 const API_URL = import.meta.env.VITE_API_URL as string;
 
 export const getBTCData = async () => {
-    return axios.get(API_URL).then((response) => response.data).catch(() => {
+    return axios.get(API_URL).then((response) => {
+        let sortedData = quickSort(response.data);
+        return sortedData;
+    }).catch(() => {
         alert(`Could not retrieve BTC information`)
     });
 }
